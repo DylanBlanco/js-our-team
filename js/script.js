326002c5962d717ddde1eccfc68938824859676a
DylanBlanco <dylanblanco1198@gmail.com>
2024-07-26 02:04:20 +0200

    stampa su console valori di ogni membro

--- js/script.js
+++ js/script.js
@@ -2,26 +2,37 @@ const teamMembers = [
     {
         name: "Wayne Barnett",
         role: "Founder & CEO",
-        image: "wayne-barnett-founder-ceo.jpg"
+        image: "wayne-barnett-founder-ceo.jpg",
     },
     {
-        name: "Wayne Barnett",
-        role: "Founder & CEO",
-        image: "wayne-barnett-founder-ceo.jpg"
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        image: "angela-caroll-chief-editor.jpg",
     },
     {
-        name: "Wayne Barnett",
-        role: "Founder & CEO",
-        image: "wayne-barnett-founder-ceo.jpg"
+        name: "Walter Gordon",
+        role: "Office Manager",
+        image: "walter-gordon-office-manager.jpg",
     },
     {
-        name: "Wayne Barnett",
-        role: "Founder & CEO",
-        image: "wayne-barnett-founder-ceo.jpg"
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        image: "angela-lopez-social-media-manager.jpg",
     },
     {
-        name: "Wayne Barnett",
-        role: "Founder & CEO",
-        image: "wayne-barnett-founder-ceo.jpg"
+        name: "Scott Estrada",
+        role: "Developer",
+        image: "scott-estrada-developer.jpg",
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        image: "barbara-ramos-graphic-designer.jpg",
     }
 ]
+
+teamMembers.forEach(function(members) {
+    console.log(`Name: ${members.name}`);
+    console.log(`Role: ${members.role}`);
+    console.log(`Image: ${members.image}`);
+    });
